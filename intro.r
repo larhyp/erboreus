edf0f8186e8f4e89769b5c18355e251a4b11e6d9
larhyp <johannes.raja@gmail.com>
2015-07-02 15:44:34 +0300

    Minor change

--- intro.r
+++ intro.r
@@ -4,7 +4,6 @@ data(state)
 states <- data.frame(state.x77, state.center)
 states <- states[states$x > -121,]
 coordinates(states) <- c("x", "y")
-states
 proj4string(states) <- CRS("+proj=longlat +ellps=clrk66")
 summary(states)
 states
